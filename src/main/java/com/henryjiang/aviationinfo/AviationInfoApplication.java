eacdb66d974b6f4175d5ece6087a0892d76422e7
HenryJiang97 <jianghanhua97@hotmail.com>
2021-02-07 16:06:23 -0800

    Initialized project and connection with MongoDB

---
+++ src/main/java/com/henryjiang/aviationinfo/AviationInfoApplication.java
@@ -0,0 +1,13 @@
+package com.henryjiang.aviationinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henryjiang/aviationinfo/AviationInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henryjiang.aviationinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviationInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
